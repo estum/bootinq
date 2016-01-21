710540921a6ebc4e3ba3e582be4d3cf9a4f3aa82
Anton <anton.estum@gmail.com>
2016-01-21 05:00:29 +0300

    Reimplement Bootinq::Component

--- lib/bootinq/component.rb
+++ lib/bootinq/component.rb
@@ -1,20 +1,64 @@
 class Bootinq
-  class Component < DelegateClass(Symbol)
-    attr_reader :gem_name, :mountable, :namespace, :group
-    alias :to_s :gem_name
-    alias :mountable? :mountable
+  class Component
+    attr_reader :intern, :id2name
 
-    def initialize(name, mountable: false)
-      super(name)
-      @gem_name  = name.to_s.freeze
-      @mountable = !!mountable
-      @group     = :"#{gem_name}_boot"
-      @namespace = :"#{gem_name.camelcase}" if mountable?
+    alias :to_sym   :intern
+    alias :to_s     :id2name
+    alias :gem_name :id2name
+    alias :name     :id2name
+
+    def initialize(intern)
+      @intern  = intern.to_sym
+      @id2name = intern.to_s.freeze
       freeze
     end
 
+    def mountable?
+      false
+    end
+
+    def group
+      :"#@id2name\_boot"
+    end
+
+    def == other
+      case other
+      when String then other == @id2name
+      when Symbol then other == @intern
+                  else super
+      end
+    end
+
+    def inspect
+      @intern.inspect
+    end
+
     def engine
-      Object.const_get(@namespace)::Engine
+      nil
+    end
+
+    def module_name
+      @id2name.camelcase.to_sym
+    end
+
+    def respond_to_missing?(method_name, include_all=false)
+      @intern.respond_to?(method_name, include_all)
+    end
+
+    private
+
+    def method_missing(method_name, *args, &blk)
+      @intern.respond_to?(method_name) ? @intern.public_send(method_name, *args, &blk) : super
+    end
+  end
+
+  class Mountable < Component
+    def mountable?
+      true
+    end
+
+    def engine
+      Object.const_get(module_name)::Engine
     end
   end
 end
--- spec/bootinq/component_spec.rb
+++ spec/bootinq/component_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
-describe Bootinq::Component do
+RSpec.describe Bootinq do
+  shared_examples 'component' do
+    let(:component) { described_class.new(name) }
+
+    it { expect(component).to eq(name.to_s) & eq(name.to_sym) }
+
+    describe '#initialize' do
+      it { expect(component).to be_frozen }
+    end
+
+    describe '#name' do
+      it { expect(component.name).to be_frozen & eq(name.to_s) }
+    end
+
+    describe '#mountable?' do
+      it { expect(component.mountable?).to be mountable }
+    end
+
+    describe '#engine' do
+      it { expect(component.engine).to eq engine }
+    end
+  end
+
+  describe Bootinq::Component do
+    let!(:name)      { :shared }
+    let!(:mountable) { false }
+    let!(:engine)    { nil }
+    it_behaves_like 'component'
+  end
+
+  describe Bootinq::Mountable do
+    let!(:name)      { :api }
+    let!(:mountable) { true }
+    let!(:engine)    { Api::Engine }
+    it_behaves_like 'component'
+  end
 end
